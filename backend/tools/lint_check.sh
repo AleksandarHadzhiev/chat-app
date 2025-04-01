d4e7f534bceb3656de486c3074d6c28709ad045d
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-04-01 15:12:18 +0200

    run bash in workflow

--- backend/tools/lint_check.sh
+++ backend/tools/lint_check.sh
@@ -3,6 +3,8 @@
 # Navigate to the backend folder
 cd ..
 
+pipenv shell
+
 # Run isort check on code
 echo "____________________ISORT_CHECK____________________"
 isort . -c -v
@@ -10,3 +12,8 @@ isort . -c -v
 # Run black check on code
 echo "____________________BLACK_CHECK____________________"
 black . --check
+
+
+# Run pylint check on code
+echo "____________________PYLINT_CHECK____________________"
+pylint .
