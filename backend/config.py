a5d4eb7682608d26e99612c540b7ddc8550621e0
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-04-01 19:44:55 +0200

    added docker environment as a configuration possibility

--- backend/config.py
+++ backend/config.py
@@ -2,12 +2,13 @@ class Config:
     DATABASE_URL = ""
     FRONTEND_URL = ""
     FRONTEND_URL = ""
+    ENVIRONMENT = ""
 
 
 class ConfigFactory:
     def __init__(self, type="dev"):
         self.type = type
-        self.types = {"dev": DevConfig(), "test": TestConfig()}
+        self.types = {"dev": DevConfig(), "test": TestConfig(), "docker": DockerConfig()}
 
     def get_config(self) -> Config:
         if self.type in self.types:
@@ -15,14 +16,29 @@ class ConfigFactory:
 
 
 class DevConfig(Config):
+    ENVIRONMENT = "dev"
+    
     USERNAME = "user"
     PASSWORD = "user"
+    HOST = "localhost"
+    DATABASE_PORT = "5432"
+    DATABASE_DB = "users"
+    ENVIRONMENT = "dev"
     FRONTEND_URL = "https://localhost:3000"
-    DATABASE_URL = f"postgresql://postgres:{USERNAME}#{PASSWORD}@localhost/dev"
-
 
 class TestConfig(Config):
-    USERNAME = "test"
-    PASSWORD = "test"
+    ENVIRONMENT = "test"
+    
     FRONTEND_URL = "https://localhost:3000"
-    DATABASE_URL = f"sqlite:///"
+    DATABASE_URL = f"sqlite:///"    
+
+
+class DockerConfig(Config):
+    ENVIRONMENT = "docker"
+
+    HOST = "database"
+    FRONTEND_URL = "https://localhost:3000" # When the dockerization is complete will be changed
+    DATABASE_NAME = "employees"
+    DATABASE_USER = "user"
+    DATABASE_PASSWORD = "password"
+    PORT_NUMBER = 3306 
