4147b2e97c3c9306acdb4439fe3dd456453b37e4
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-04-02 11:52:40 +0200

    initial version of entities used in the app

---
+++ backend/src/modules/message.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+class Message:
+    def __init__(self, author, user_id,  content, group_id):
+        self.author = author
+        self.user_id = user_id
+        self.content = content
+        self.group_id = group_id
+        self.date_sent = datetime.now()
+
+
+    def fetch_from_db(self, id, author, user_id, content, group_id, date_sent):
+        self.id = id
+        self.author = author
+        self.user_id = user_id
+        self.content = content
+        self.group_id = group_id
+        self.date_sent = date_sent
