82628f526e62a231b601268c5d1ed84bac22cd5a
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-05-21 14:26:23 +0200

    set websocket to be usable in page for groups

--- frontend/src/app/groups/page.tsx
+++ frontend/src/app/groups/page.tsx
@@ -1,9 +1,25 @@
+"use client"
+
 import Groups from "@/components/Groups/Groups"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
 export default function GroupsPage() {
+    const [socket, setSocket] = useState<WebSocket>()
+    const [_url, setUrl] = useState()
+    useEffect(() => {
+        console.log(localStorage.getItem("ws"))
+        const urlFromStorage = localStorage.getItem("ws")
+        if (urlFromStorage != undefined && socket == undefined) {
+            setUrl(_url)
+            const ws = new WebSocket(urlFromStorage)
+            setSocket(ws)
+        }
+        console.log(socket)
+    }, [])
+
     return (
         <div className="flex flex-col w-full h-full">
-            <Groups />
+            {socket && socket != undefined ? <Groups socket={socket} /> : null}
         </div>
     )
 }
