df4b1b7b36def1bbcb77e9016133d51b95b32bc9
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-06-01 14:30:09 +0200

    add default password to starting file

--- backend/app.py
+++ backend/app.py
@@ -9,15 +9,15 @@ from src.main import create_app
 
 n = len(sys.argv)
 environments = ["docker", "dev", "test"]
-if n != 3:
-    logging.error(f"Unsupported arguments: {n}")
-else:
-    env = sys.argv[1]
+
+env = sys.argv[1]
+password="secret-key-placeholder"
+if n == 3:
     password = sys.argv[2]
-    if env in environments:
-        response = create_app(server=env, password=password)
-        app = response["app"]
-        config = response["config"]
-        uvicorn.run(app=app, host=config.BASE_URL)
-    else:
-        logging.error(f"Unsupported environment: {env}")
+if env in environments:
+    response = create_app(server=env, password=password)
+    app = response["app"]
+    config = response["config"]
+    uvicorn.run(app=app, host=config.BASE_URL)
+else:
+    logging.error(f"Unsupported environment: {env}")
